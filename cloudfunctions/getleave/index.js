3c9bf7ac6bdca41dea4ceac9eabcb68b2f33467f
173xs <1730384353@qq.com>
2020-09-06 17:30:42 +0800

    feat:查询请假单的云函数完成
    老师和学生的查询都是getleave函数，通过funcName选择不同的查询方式。

--- cloudfunctions/getleave/index.js
+++ cloudfunctions/getleave/index.js
@@ -8,22 +8,53 @@ const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
+  switch (event.funcName) {
+    case '1-a':
+      return await getLeave1_a(event, context);
+      break;
+    case '2-a':
+      return await getLeave2_a(event, context);
+      break;
+    case '2-b':
+      return await getLeave2_b(event, context);
+      break;
+    case '2-c':
+      return await getLeave2_c(event, context);
+      break;
+  }
 
+}
+
+//通过openid查学号
+async function getSno() {
+  const wxContext = cloud.getWXContext()
+
+  return await db.collection("stuInfo")
+    .where({
+      _openid: wxContext.OPENID,
+    })
+    .field({
+      sno: true,
+    })
+    .get()
+}
+//老师审批请假单的查询函数
+async function getLeave1_a(event, context) {
   let leave = await db.collection("leave")
-  .where({
-    'approveState':0
-  })
-  .get()
+    .where({
+      'approveState': 0
+    })
+    .get()
 
   //console.log('leave = ',leave)
-  for ( let item of leave.data){
+  for (let item of leave.data) {
     let leave_sno = item.sno
     //console.log('leave_sno = ',leave_sno)
     let stu_info = await db.collection("stuInfo")
-    .where({
-      sno:leave_sno
-    })
-    .get()
+      .where({
+        sno: leave_sno
+      })
+      .get()
     //console.log('stu_info = ',stu_info)
     item['sname'] = stu_info.data[0].sname
     item['sacademy'] = stu_info.data[0].sacademy
@@ -31,4 +62,81 @@ exports.main = async (event, context) => {
   }
   //console.log(leave.data)
   return leave
+}
+
+//查询学生的未审批请假单
+async function getLeave2_a(event, context) {
+  console.log('get sno')
+  let sno
+  await getSno()
+  .then(res=>{
+    // console.log('res = ',res)
+    sno = res.data[0].sno
+  })
+  .catch(err=>{
+    // console.log(err)
+    return err
+  })
+
+  console.log('sno = ',sno)
+  return await db.collection("leave")
+    .where(_.and([{
+        'approveState': _.eq(0)
+      },
+      {
+        'sno': _.eq(sno)
+      }
+    ]))
+    .get()
+}
+
+//查询已通过审批请假单
+async function getLeave2_b(event, context) {
+  let sno
+  await getSno()
+  .then(res=>{
+    // console.log('res = ',res)
+    sno = res.data[0].sno
+  })
+  .catch(err=>{
+    // console.log(err)
+    return err
+  })
+
+  return await db.collection("leave")
+    .where(_.and([{
+        'approveState': _.eq(1) //已经通过审批
+      },
+      {
+        'sno': _.eq(sno)
+      },
+      {
+        'checkState': _.eq(0) //通过审批但是没有使用
+      }
+    ]))
+    .get()
+}
+
+//查询已经驳回的请假申请
+async function getLeave2_c(event, context) {
+  let sno
+  await getSno()
+  .then(res=>{
+    // console.log('res = ',res)
+    sno = res.data[0].sno
+  })
+  .catch(err=>{
+    // console.log(err)
+    return err
+  })
+
+  return await db.collection("leave")
+    .where(_.and([{
+        'approveState': _.eq(-1) //驳回的请假单
+      },
+      {
+        'sno': _.eq(sno)
+      }
+    ]))
+    .get()
 }
--- miniprogram/pages/myHome/myLeave.js
+++ miniprogram/pages/myHome/myLeave.js
@@ -5,62 +5,112 @@ Page({
    * 页面的初始数据
    */
   data: {
-    coverBoxDisplay:"none",
+    coverBoxDisplay: "none",
 
     //菜单栏按钮选中时的样式
-    noCheckWxss:'addColor',
-    yesCheckWxss:' ',
-    backCheckWxss:' '
+    noCheckWxss: 'addColor',
+    yesCheckWxss: ' ',
+    backCheckWxss: ' '
   },
+
   //未审核按钮点击事件
-  noCheckClick:function(){
+  noCheckClick: function () {
+    console.log('nocheckclick')
     //切换按钮颜色
     this.setData({
-      noCheckWxss:'addColor',
-      yesCheckWxss:' ',
-      backCheckWxss:' '
+      noCheckWxss: 'addColor',
+      yesCheckWxss: ' ',
+      backCheckWxss: ' '
     })
     //刷新列表，填充未审核的请假信息
+    wx.cloud.callFunction({
+        name: 'getleave',
+        data: {
+          funcName: '2-a'
+        }
+      })
+      .then(res => {
+        console.log('未审核的请假单', res)
+      })
+      .catch(err => {
+
+      })
   },
 
   //已审核按钮点击事件
-  yesCheckClick:function(){
+  yesCheckClick: function () {
     //切换按钮颜色
     this.setData({
-      noCheckWxss:'',
-      yesCheckWxss:'addColor',
-      backCheckWxss:' '
+      noCheckWxss: '',
+      yesCheckWxss: 'addColor',
+      backCheckWxss: ' '
     })
     //刷新列表，填充已审核的请假信息
+    wx.cloud.callFunction({
+        name: 'getleave',
+        data: {
+          funcName: '2-b'
+        }
+      })
+      .then(res => {
+        console.log('已审核的请假单', res)
+      })
+      .catch(err => {
+
+      })
   },
 
-    //驳回按钮点击事件
-  backCheckClick:function(){
+  //驳回按钮点击事件
+  backCheckClick: function () {
     //切换按钮颜色
     this.setData({
-      noCheckWxss:'',
-      yesCheckWxss:' ',
-      backCheckWxss:'addColor'
+      noCheckWxss: '',
+      yesCheckWxss: ' ',
+      backCheckWxss: 'addColor'
     })
     //刷新列表，填充驳回的请假信息
+    wx.cloud.callFunction({
+        name: 'getleave',
+        data: {
+          funcName: '2-c'
+        }
+      })
+      .then(res => {
+        console.log('驳回的请假单', res)
+      })
+      .catch(err => {
+
+      })
   },
 
 
-  showBill:function(){
+  showBill: function () {
     this.setData({
-      coverBoxDisplay:"block"
+      coverBoxDisplay: "block"
     })
   },
-  hideBill:function(){
+  hideBill: function () {
     this.setData({
-      coverBoxDisplay:"none"
+      coverBoxDisplay: "none"
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //默认刷新未审核的
+    wx.cloud.callFunction({
+      name: 'getleave',
+      data: {
+        funcName: '2-a'
+      }
+    })
+    .then(res => {
+      console.log('未审核的请假单', res)
+    })
+    .catch(err => {
 
+    })
   },
 
   /**
