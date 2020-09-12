78e556cccfab609229f47fb924d99c11e9f1eeac
173xs <1730384353@qq.com>
2020-09-12 21:13:34 +0800

    fix:修复approveLeave
    1.添加审核人no
    2.审核通过添加使用状态(checkState)

--- cloudfunctions/approveLeave/index.js
+++ cloudfunctions/approveLeave/index.js
@@ -6,14 +6,26 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
+  let tno
+  //先获取老师的no
+  await db.collection("teaInfo")
+  .where({
+    _openid: wxContext.OPENID
+  })
+  .get()
+  .then(res=>{
+    tno = res.data[0].tno
+  })
 
-  let data 
-  data.approveState = event.approveState
+  console.log(event)
+  let data = {}
+  data['approveState'] = event.approveState
   if (event.approveState == 1){
-    data.checkState = 0
+    data['checkState'] = 0
   }
-  return await db.collection('leave').
-  doc(event.leaveId)
+  data.ano = tno
+  return await db.collection('leave')
+  .doc(event.leaveId)
   .update({
     data: data
   })
