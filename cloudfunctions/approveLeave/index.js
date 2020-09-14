fdf2ff5d905ae7814f39ea09a87ee9f16f27d87a
173xs <1730384353@qq.com>
2020-09-14 08:56:26 +0800

    fix:审批通过请假单时checkState首先应该等于-1

--- cloudfunctions/approveLeave/index.js
+++ cloudfunctions/approveLeave/index.js
@@ -5,14 +5,13 @@ cloud.init()
 const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
 
   console.log(event)
   let data = {}
   data['approveState'] = event.approveState
-  //如果是审批通过，就加上一个使用状态字段
+  //如果是审批通过，就加上一个使用状态字段，-1表示未使用，未出校门
   if (event.approveState == 1){
-    data['checkState'] = 0
+    data['checkState'] = -1
   }
   data.ano = event.tno
   return await db.collection('leave')
