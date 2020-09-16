2612b75795105499d72e9d5fb458c524329eb5ac
173xs <1730384353@qq.com>
2020-09-16 11:22:15 +0800

    feat:我的消息功能完成

--- cloudfunctions/getMsg/index.js
+++ cloudfunctions/getMsg/index.js
@@ -36,9 +36,9 @@ exports.main = async (event, context) => {
             subDate: 0,
             reviewDate: 0,
             rno: 0,
-            //leaveDate: 0,
-            leaveReason: 0,
-            //returnDate: 0,
+            // leaveDate: 0,
+            // leaveReason: 0,
+            // returnDate: 0,
             ano: 0,
             sno: 0
           })
@@ -60,19 +60,33 @@ exports.main = async (event, context) => {
       type: foo.list[0].type,
       id: foo.list[0].id,
       time: foo.list[0].checkTime,
-      tname: foo.list[0].tname
+      tname: foo.list[0].tname,
     }
+    let detail = {}
     if ('leave' == item.type) {
-      bar.title = foo.list[0].leaveClass +
-        ':' +
-        foo.list[0].leaveDate +
-        '~' +
-        foo.list[0].returnDate //请假类型:离校时间~返校时间
-      bar.checkState = foo.list[0].approveState
+      detail = {
+        leaveClass: foo.list[0].leaveClass,
+        leaveDate: foo.list[0].leaveDate,
+        returnDate: foo.list[0].returnDate,
+        leaveReason: foo.list[0].leaveReason,
+        approveState: foo.list[0].approveState,
+        checkState: foo.list[0].checkState
+      }
+      // bar.title = foo.list[0].leaveClass +
+      //   ':' +
+      //   foo.list[0].leaveDate +
+      //   '~' +
+      //   foo.list[0].returnDate //请假类型:离校时间~返校时间
+      // bar.checkState = foo.list[0].approveState
     } else {
-      bar.title = foo.list[0].title
-      bar.checkState = 1
+      detail = {
+        title: foo.list[0].title,
+        //checkState: 1
+      }
+      // bar.title = foo.list[0].title
+      // bar.checkState = 1
     }
+    bar.detail = detail
     data.push(bar)
     //console.log('foo = ', foo)
   }
--- miniprogram/pages/myHome/inform.js
+++ miniprogram/pages/myHome/inform.js
@@ -1,21 +1,30 @@
 // pages/inform/inform.js
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    coverBoxDisplay: "none",
     curLeaveBill: [], //初始化请假单的信息
   },
   showBill: function (e) {
-    var curId = e.currentTarget.dataset.curid
+    console.log(e)
     /*
-    这里根据curId获取一下请假单的信息bill
+    这里不用获取id了，直接把所以信息都带过来了，
+    反正就只差一点点信息了。
     */
+    let bill = this.data.reslist[e.currentTarget.dataset.curidx]
+    bill.sno = app.globalData.regInfo.sno
+    bill.sname = app.globalData.regInfo.sname
+    bill.sacademy = app.globalData.regInfo.sacademy
+
     this.setData({
       coverBoxDisplay: "block",
       curLeaveBill: bill
     })
+    console.log(this.data.curLeaveBill)
   },
   hideBill: function () {
     this.setData({
@@ -26,7 +35,31 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.cloud.callFunction({
+        name: 'getMsg',
+        data: {
+          sno: app.globalData.regInfo.sno,
+        }
+      })
+      .then(res => {
+        console.log('消息列表', res.result)
+        let list = []
+        for (var item of res.result) {
+          if ('leave' == item.type) {
+            item.title = item.detail.leaveClass +
+              ':' +
+              item.detail.leaveDate +
+              '~' +
+              item.detail.returnDate
+          } else {
+            item.title
+          }
+          list.push(item)
+        }
+        this.setData({
+          reslist: list
+        })
+      })
   },
 
   /**
