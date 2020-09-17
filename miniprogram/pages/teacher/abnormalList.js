c60dfcaa71a32963e9b02adb878381dc085a387e
173xs <1730384353@qq.com>
2020-09-17 15:19:31 +0800

    feat:获取发热人员名单完成

--- miniprogram/pages/teacher/abnormalList.js
+++ miniprogram/pages/teacher/abnormalList.js
@@ -1,29 +1,57 @@
 // miniprogram/pages/teacher/abnormalList.js
 var util = require('../../utils/util.js')
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    nowDate:"",
-    queryDate:"",
+    nowDate: "",
+    queryDate: "",
+    reslist: []
   },
-  bindDateChange: function(e) {
+  bindDateChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       queryDate: e.detail.value,
     })
+    this.callGetTemp(this.data.queryDate)
+  },
+  //调用getTemp云函数
+  callGetTemp(queryDate) {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+    wx.cloud.callFunction({
+        name: 'getTemp',
+        data: {
+          funcName: 2,
+          date: queryDate,
+          academy: app.globalData.regInfo.tacademy
+        }
+      })
+      .then(res => {
+        console.log('res = ', res)
+        wx.hideLoading({
+          success: (res) => {},
+        })
+        this.setData({
+          reslist: res.result.list
+        })
+      })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var now=util.formatDay(new Date())
+    var now = util.formatDay(new Date())
     this.setData({
-      nowDate:now,
+      nowDate: now,
       queryDate: now
     })
+    this.callGetTemp(this.data.queryDate)
   },
 
   /**
