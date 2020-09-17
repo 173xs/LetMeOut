5ef1d6f0f0a0592b61cd4ca63cc7a4776e0f827b
173xs <1730384353@qq.com>
2020-09-17 11:29:43 +0800

    bug:选取时间后重新获取体温概览数据
    封装调用云函数获取概览数据的函数

--- miniprogram/pages/teacher/tempShow.js
+++ miniprogram/pages/teacher/tempShow.js
@@ -17,12 +17,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var now = util.formatDay(new Date())
-    this.setData({
-      nowDate: now,
-      queryDate: now
-    })
+  callGetTemp(queryDate) {
     wx.showLoading({
       title: '加载中',
       mask: true
@@ -30,12 +25,12 @@ Page({
     wx.cloud.callFunction({
         name: 'getTemp',
         data: {
-          date: now,
+          date: queryDate,
           academy: app.globalData.regInfo.tacademy
         }
       })
       .then(res => {
-        console.log('res = ',res)
+        console.log('res = ', res)
         wx.hideLoading({
           success: (res) => {},
         })
@@ -46,55 +41,66 @@ Page({
         })
       })
   },
+  onLoad: function (options) {
+    var now = util.formatDay(new Date())
+    this.setData({
+      nowDate: now,
+      queryDate: now
+    })
+    this.callGetTemp(this.data.queryDate)
+  },
   bindDateChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       queryDate: e.detail.value,
     })
     //这里获取日期对应的出行记录 eg: queryDate:2020-09-10
+    this.callGetTemp(this.data.queryDate)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {//画饼图
-        // 页面渲染完成
-        //使用wx.createContext获取绘图上下文context
-        var context = wx.createContext();
-        // 画饼图
-        //    数据源
-        var rate=1-(this.data.hotNum/this.data.upedNum).toFixed(2)
-        var colors = [ "#68ed50", "#f6b943"];
-        //    定义圆心坐标
-        var point = { x: 100, y: 100 };
-        //    定义半径大小
-        var radius = 60;
-        /*    循环遍历所有的pie */
-        for( var i = 0;i < 2;i++ ) {
-            context.beginPath();
-            //    	1.起点弧度
-            var start = 0;
-            //   	2.画一条弧，并填充成三角饼pie，前2个参数确定圆心，第3参数为半径，第4参数起始旋转弧度数，第5参数本次扫过的弧度数，第6个参数为时针方向-false为顺时针
-            if(i==0){
-                context.arc( point.x, point.y, radius, start, rate * 2 * Math.PI, false );
-            }
-            else{
-                start=rate * 2 * Math.PI;   
-                context.arc( point.x, point.y, radius, start, 2 * Math.PI, false );
-            }
-            //      3.连线回圆心
-            context.lineTo( point.x, point.y );
-            //      4.填充样式
-            context.setFillStyle( colors[ i ] );
-            //      5.填充动作
-            context.fill();
-            context.closePath();
-        }
-        //调用wx.drawCanvas，通过canvasId指定在哪张画布上绘制，通过actions指定绘制行为
-        wx.drawCanvas( {
-            //指定canvasId,canvas 组件的唯一标识符
-            canvasId: 'mypie',
-            actions: context.getActions()
-        });
+  onReady: function () { //画饼图
+    // 页面渲染完成
+    //使用wx.createContext获取绘图上下文context
+    var context = wx.createContext();
+    // 画饼图
+    //    数据源
+    var rate = 1 - (this.data.hotNum / this.data.upedNum).toFixed(2)
+    var colors = ["#68ed50", "#f6b943"];
+    //    定义圆心坐标
+    var point = {
+      x: 100,
+      y: 100
+    };
+    //    定义半径大小
+    var radius = 60;
+    /*    循环遍历所有的pie */
+    for (var i = 0; i < 2; i++) {
+      context.beginPath();
+      //    	1.起点弧度
+      var start = 0;
+      //   	2.画一条弧，并填充成三角饼pie，前2个参数确定圆心，第3参数为半径，第4参数起始旋转弧度数，第5参数本次扫过的弧度数，第6个参数为时针方向-false为顺时针
+      if (i == 0) {
+        context.arc(point.x, point.y, radius, start, rate * 2 * Math.PI, false);
+      } else {
+        start = rate * 2 * Math.PI;
+        context.arc(point.x, point.y, radius, start, 2 * Math.PI, false);
+      }
+      //      3.连线回圆心
+      context.lineTo(point.x, point.y);
+      //      4.填充样式
+      context.setFillStyle(colors[i]);
+      //      5.填充动作
+      context.fill();
+      context.closePath();
+    }
+    //调用wx.drawCanvas，通过canvasId指定在哪张画布上绘制，通过actions指定绘制行为
+    wx.drawCanvas({
+      //指定canvasId,canvas 组件的唯一标识符
+      canvasId: 'mypie',
+      actions: context.getActions()
+    });
   },
 
   /**
